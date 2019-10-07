457701e18bfffe8cc66ca16118e7c715e37645b5
Betty <betty.bondoc@pino.ph>
2019-10-07 14:37:09 +0800

    setup local env

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-width=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<title>Sigla</title>
+
+	<!-- Favicon -->
+	<link rel="shortcut icon" href="assets/img/icons/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="assets/img/icons/favicon.ico" type="image/x-icon">
+
+	<!-- Fonts -->
+
+
+	<!-- Custom CSS -->
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+</head>
+<body>
+
+	<p class="light">Light</p>
+	<p class="medium">Medium</p>
+	<p class="regular">Regular</p>
+	<p class="heading">Canopee</p>
+	<!-- Greensock -->
+	<script type="text/javascript" src="assets/js/greensock-js-2.1.3/src/minified/TweenMax.min.js"></script>
+	<script type="text/javascript" src="assets/js/greensock-js-2.1.3/src/minified/TimelineMax.min.js"></script>
+
+	<!-- Scrollmagic -->
+	<script type="text/javascript" src="assets/js/scrollmagic-2.0.7/scrollmagic/uncompressed/ScrollMagic.js"></script>
+	<script type="text/javascript" src="assets/js/scrollmagic-2.0.7/scrollmagic/uncompressed/plugins/debug.addIndicators.js"></script>
+	<script type="text/javascript" src="assets/js/scrollmagic-2.0.7/scrollmagic/uncompressed/plugins/animation.gsap.js"></script>
+	
+
+	<!-- Custom JS -->
+	<script type="text/javascript" src="assets/js/custom.js"></script>
+
+</body>
+</html>
