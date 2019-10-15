5d461ff5fd5d1e728df6f4aaf035bc7509c41063
Pino Studio <betty.bondoc@pino.ph>
2019-10-15 18:45:44 +0800

    work on homepage large screens

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<div id="content-container">
 		<div class="flex-container featured-article">
 			<div class="content">
-				<div class="img-container hero medium-and-down">
+				<div class="img-container hero portrait">
 					<img src="assets/img/placeholders/1.jpg" alt="">
 				</div>
 
@@ -19,40 +19,44 @@
 					</div>
 				</div>
 				
-				<div class="img-container hero large-and-up">
+				<div class="img-container hero landscape">
 					<img src="assets/img/placeholders/1.jpg" alt="">
 				</div>
 			</div>
 		</div>
 
 		<div class="flex-container about">
-			<div class="wrapper">
-				<div class="row">
-					<div class="col col-md-50 col-lg-60">
-						<div class="images">
-							<div class="img-container">
-								<img src="assets/img/placeholders/1.jpg" alt="">
-							</div>
-							<div class="img-container">
-								<img src="assets/img/placeholders/1.jpg" alt="">
-							</div>
-							<div class="img-container">
-								<img src="assets/img/placeholders/1.jpg" alt="">
+			<div class="content">
+				<div class="wrapper">
+					<div class="row">
+						<div class="col col-md-50 col-lg-60">
+							<div class="images">
+								<div class="img-container">
+									<img src="assets/img/placeholders/1.jpg" alt="">
+								</div>
+								<div class="img-container">
+									<img src="assets/img/placeholders/1.jpg" alt="">
+								</div>
+								<div class="img-container">
+									<img src="assets/img/placeholders/1.jpg" alt="">
+								</div>
 							</div>
 						</div>
-					</div>
-					<div class="col col-md-50 col-lg-40">
-						<div class="text">
-							<h3>
-								Jovy Acuzar is Sigla. Lorem ipsum dolor sit amet huest, consectetur aqua sed do  adipiscing elit.
-							</h3>
-							<p>
-								Lorem ipsum dolor sit amet loremu, consectetur adipiscing elit, sed do eius mod tempor incididunt ut labore et dolore magna aliqua.
-							</p>
-							<div class="btn-container">
-								<a href="about.php">
-									<img src="assets/img/icons/arrow.svg">
-								</a>
+						<div class="col col-md-50 col-lg-40">
+							<div class="text-container">
+								<div class="text">
+									<h3>
+										Jovy Acuzar is Sigla. Lorem ipsum dolor sit amet huest, consectetur aqua sed do  adipiscing elit.
+									</h3>
+									<p>
+										Lorem ipsum dolor sit amet loremu, consectetur adipiscing elit, sed do eius mod tempor incididunt ut labore et dolore magna aliqua.
+									</p>
+									<div class="btn-container">
+										<a href="about.php">
+											<img src="assets/img/icons/arrow.svg">
+										</a>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
@@ -60,70 +64,149 @@
 			</div>
 		</div>
 		
-		<div class="flex-container recipes">
-			<div class="row">
-				<div class="col col-lg-50">
-					<div class="background-container">
-						<div class="primary-heading-group">
-							<h3 class="label">Recipe</h3>
-							<h2 class="heading">Sayote Crumble</h2>
-							<h4 class="label"> — Juana Yupangco</h4>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-							consequat.</p>
-							<div class="btn-container">
-								<a href="recipe.php">
-									<img src="assets/img/icons/arrow.svg">
-								</a>
+		<!-- <div class="flex-container recipes">
+			<div class="content">
+				<div class="row">
+					<div class="col col-lg-50">
+						<div class="background-container">
+							<div class="primary-heading-group">
+								<h3 class="label">Recipe</h3>
+								<h2 class="heading">Sayote Crumble</h2>
+								<h4 class="label"> — Juana Yupangco</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+								consequat.</p>
+								<div class="btn-container">
+									<a href="recipe.php">
+										<img src="assets/img/icons/arrow.svg">
+									</a>
+								</div>
 							</div>
-						</div>
-					</div>					
-					<div class="img-container hero">
-						<img src="assets/img/placeholders/1.jpg" alt="">
-					</div>
-				</div>
-				<div class="col col-lg-50  thumbnail-group">
-					<div class="row wrapper">
-						<div class="img-container thumbnail">
+						</div>					
+						<div class="img-container hero">
 							<img src="assets/img/placeholders/1.jpg" alt="">
 						</div>
-						<div class="primary-heading-group">
-							<h3 class="label">Recipe</h3>
-							<h2 class="sans-heading">Sayote Crumble</h2>
-							<h4 class="label"> — Juana Yupangco</h4>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor.</p>
-							<div class="btn-container">
-								<a href="index.php">
-									<img src="assets/img/icons/arrow.svg">
-								</a>
+					</div>
+					<div class="col col-lg-50 thumbnail-group">
+						<div class="row wrapper">
+							<div class="col col-sm-30 col-md-40 col-lg-50">
+								<div class="img-container thumbnail">
+									<img src="assets/img/placeholders/2.jpg" alt="">
+								</div>
+							</div>
+							<div class="col col-sm-70 col-md-60 col-lg-50">
+								<div class="primary-heading-group">
+									<h3 class="label">Sweets & Snacks</h3>
+									<h2 class="sans-heading">Lorem Ipsum</h2>
+									<h4 class="label"> — Juana Yupangco</h4>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor.</p>
+									<div class="btn-container">
+										<a href="index.php">
+											<img src="assets/img/icons/arrow.svg">
+										</a>
+									</div>
+								</div>
 							</div>
 						</div>
-					</div>
-					<hr>
-					<div class="row wrapper">
-						<div class="img-container thumbnail">
-							<img src="assets/img/placeholders/1.jpg" alt="">
+						<div class="row divider">
+							<hr>
 						</div>
-						<div class="primary-heading-group">
-							<h3 class="label">Recipe</h3>
-							<h2 class="sans-heading">Sayote Crumble</h2>
-							<h4 class="label"> — Juana Yupangco</h4>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor.</p>
-							<div class="btn-container">
-								<a href="index.php">
-									<img src="assets/img/icons/arrow.svg">
-								</a>
+						<div class="row wrapper">
+							<div class="col col-sm-30 col-md-40 col-lg-50">
+								<div class="img-container thumbnail">
+									<img src="assets/img/placeholders/2.jpg" alt="">
+								</div>
+							</div>
+							<div class="col col-sm-70 col-md-60 col-lg-50">
+								<div class="primary-heading-group">
+									<h3 class="label">Lunch & Dinner</h3>
+									<h2 class="sans-heading">Lorem Ipsum</h2>
+									<h4 class="label"> — Juana Yupangco</h4>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor.</p>
+									<div class="btn-container">
+										<a href="index.php">
+											<img src="assets/img/icons/arrow.svg">
+										</a>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</div> -->
 
-		<div class="flex-container articles">
-			
-		</div>
+		<!-- <div class="flex-container articles">
+			<div class="content">
+				<div class="row">
+					<div class="col col-lg-50">
+						<div class="img-container hero">
+							<img src="assets/img/placeholders/1.jpg" alt="">
+						</div>
+						<div class="background-container">
+							<div class="primary-heading-group">
+								<h3 class="label">Mindful Living</h3>
+								<h2 class="heading">Lorem Ipsum</h2>
+								<h4 class="label"> — Juana Yupangco</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+								consequat.</p>
+								<div class="btn-container">
+									<a href="recipe.php">
+										<img src="assets/img/icons/arrow.svg">
+									</a>
+								</div>
+							</div>
+						</div>					
+					</div>
+					<div class="col col-lg-50 thumbnail-group">
+						<div class="row wrapper">
+							<div class="col col-sm-30 col-md-40 col-lg-50">
+								<div class="img-container thumbnail">
+									<img src="assets/img/placeholders/2.jpg" alt="">
+								</div>
+							</div>
+							<div class="col col-sm-70 col-md-60 col-lg-50">
+								<div class="primary-heading-group">
+									<h3 class="label">Mindful Living</h3>
+									<h2 class="sans-heading">Lorem Ipsum</h2>
+									<h4 class="label"> — Juana Yupangco</h4>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor.</p>
+									<div class="btn-container">
+										<a href="index.php">
+											<img src="assets/img/icons/arrow.svg">
+										</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="row divider">
+							<hr>
+						</div>
+						<div class="row wrapper">
+							<div class="col col-sm-30 col-md-40 col-lg-50">
+								<div class="img-container thumbnail">
+									<img src="assets/img/placeholders/2.jpg" alt="">
+								</div>
+							</div>
+							<div class="col col-sm-70 col-md-60 col-lg-50">
+								<div class="primary-heading-group">
+									<h3 class="label">Mindful Living</h3>
+									<h2 class="sans-heading">Lorem Ipsum</h2>
+									<h4 class="label"> — Juana Yupangco</h4>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor.</p>
+									<div class="btn-container">
+										<a href="index.php">
+											<img src="assets/img/icons/arrow.svg">
+										</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div> -->
 
-		<div class="flex-container shop">
+		<!-- <div class="flex-container shop">
 			<div class="content">
 				<div class="img-container hero">
 					<img src="assets/img/placeholders/1.jpg" alt="">
@@ -131,8 +214,8 @@
 
 				<div class="background-container">
 					<div class="primary-heading-group">
-						<h2 class="heading">Note to Self: Say "No"</h2>
-						<h4 class="label"> — Jovy Acuzar, 19Aug13</h4>
+						<h2 class="heading">Toasted Coconut Butter</h2>
+						<h4 class="label"> — The Unbakery</h4>
 						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 						<div class="btn-container">
 							<a href="#" class="button medium-and-down">Buy Now</a>
@@ -140,7 +223,7 @@
 					</div>
 				</div>
 			</div>
-		</div>
+		</div> -->
 		
 	</div>
 	
